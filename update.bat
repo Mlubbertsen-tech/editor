cb0d34b128c7ae9fb781faa506e4de495a38ac67
Matthias Lubbertsen <matthiaslubbertsen@gmail.com>
2025-06-02 21:16:03 +0200

    Update: $(powershell -Command

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 git add .
 
-git commit -m "Update: $(powershell -Command "& { (git diff --name-status | % { $parts = $_ -split \"\t\"; switch ($parts[0]) { 'A' { \"added $($parts[1])\" }; 'M' { \"updated $($parts[1])\" }; 'D' { \"deleted $($parts[1])\" }; default { \"$($parts[0]) $($parts[1])\" } } }) -join ', ' }")"
+git commit -m "Update: $(powershell -Command "& { (git diff --name-status | ForEach-Object { $parts = $_ -split \"\t\"; switch ($parts[0]) { 'A' { \"added $($parts[1])\" }; 'M' { \"updated $($parts[1])\" }; 'D' { \"deleted $($parts[1])\" }; default { \"$($parts[0]) $($parts[1])\" } } }) -join ', ' }")"
 
 git push origin main
